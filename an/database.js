3c6cd3389b7dafc0bb73c5c2bd9d0dd6ca1d70f2
lamvu <nickan810@gmail.com>
2015-11-14 03:07:58 -0800

    outcome

---
+++ an/database.js
@@ -0,0 +1,159 @@
+<!--
+ SQL XML created by WWW SQL Designer, https://github.com/ondras/wwwsqldesigner/ 
+-->
+<!--  Active URL: http://localhost/sqldesigner/  -->
+<sql>
+<datatypes db="mysql">
+<group label="Numeric" color="rgb(238,238,170)">
+<type label="Integer" length="0" sql="INTEGER" quote=""/>
+<type label="TINYINT" length="0" sql="TINYINT" quote=""/>
+<type label="SMALLINT" length="0" sql="SMALLINT" quote=""/>
+<type label="MEDIUMINT" length="0" sql="MEDIUMINT" quote=""/>
+<type label="INT" length="0" sql="INT" quote=""/>
+<type label="BIGINT" length="0" sql="BIGINT" quote=""/>
+<type label="Decimal" length="1" sql="DECIMAL" re="DEC" quote=""/>
+<type label="Single precision" length="0" sql="FLOAT" quote=""/>
+<type label="Double precision" length="0" sql="DOUBLE" re="DOUBLE" quote=""/>
+</group>
+<group label="Character" color="rgb(255,200,200)">
+<type label="Char" length="1" sql="CHAR" quote="'"/>
+<type label="Varchar" length="1" sql="VARCHAR" quote="'"/>
+<type label="Text" length="0" sql="MEDIUMTEXT" re="TEXT" quote="'"/>
+<type label="Binary" length="1" sql="BINARY" quote="'"/>
+<type label="Varbinary" length="1" sql="VARBINARY" quote="'"/>
+<type label="BLOB" length="0" sql="BLOB" re="BLOB" quote="'"/>
+</group>
+<group label="Date & Time" color="rgb(200,255,200)">
+<type label="Date" length="0" sql="DATE" quote="'"/>
+<type label="Time" length="0" sql="TIME" quote="'"/>
+<type label="Datetime" length="0" sql="DATETIME" quote="'"/>
+<type label="Year" length="0" sql="YEAR" quote=""/>
+<type label="Timestamp" length="0" sql="TIMESTAMP" quote="'"/>
+</group>
+<group label="Miscellaneous" color="rgb(200,200,255)">
+<type label="ENUM" length="1" sql="ENUM" quote=""/>
+<type label="SET" length="1" sql="SET" quote=""/>
+<type label="Bit" length="0" sql="bit" quote=""/>
+</group>
+</datatypes>
+<table x="351" y="45" name="user">
+<row name="id" null="1" autoincrement="1">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+</row>
+<row name="name" null="1" autoincrement="0">
+<datatype>VARCHAR(250)</datatype>
+<default>NULL</default>
+</row>
+<row name="email" null="1" autoincrement="0">
+<datatype>VARCHAR(250)</datatype>
+<default>NULL</default>
+</row>
+<row name="password" null="1" autoincrement="0">
+<datatype>MEDIUMTEXT</datatype>
+<default>NULL</default>
+</row>
+<row name="role" null="1" autoincrement="0">
+<datatype>CHAR(50)</datatype>
+<default>NULL</default>
+</row>
+<row name="gender" null="1" autoincrement="0">
+<datatype>TINYINT</datatype>
+<default>NULL</default>
+<comment>0 men, 1 women</comment>
+</row>
+<row name="avatar" null="1" autoincrement="0">
+<datatype>MEDIUMTEXT</datatype>
+<default>NULL</default>
+</row>
+<row name="created" null="1" autoincrement="0">
+<datatype>DATETIME</datatype>
+<default>NULL</default>
+</row>
+<row name="updated" null="1" autoincrement="0">
+<datatype>DATETIME</datatype>
+<default>NULL</default>
+</row>
+<key type="PRIMARY" name="">
+<part>id</part>
+</key>
+</table>
+<table x="569" y="48" name="transaction">
+<row name="id" null="1" autoincrement="1">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+</row>
+<row name="user_id" null="1" autoincrement="0">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+<relation table="user" row="id"/>
+</row>
+<row name="from" null="1" autoincrement="0">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+<relation table="fund" row="id"/>
+<comment>from fund</comment>
+</row>
+<row name="to" null="1" autoincrement="0">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+<relation table="fund" row="id"/>
+<comment>to fund</comment>
+</row>
+<row name="content" null="1" autoincrement="0">
+<datatype>VARCHAR(250)</datatype>
+<default>NULL</default>
+</row>
+<row name="type" null="1" autoincrement="0">
+<datatype>TINYINT</datatype>
+<default>NULL</default>
+<comment>0 - Income, 1 - Outcome</comment>
+</row>
+<row name="amount" null="1" autoincrement="0">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+</row>
+<row name="created" null="1" autoincrement="0">
+<datatype>DATETIME</datatype>
+<default>NULL</default>
+</row>
+<key type="PRIMARY" name="">
+<part>id</part>
+</key>
+</table>
+<table x="865" y="35" name="fund">
+<row name="id" null="1" autoincrement="1">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+</row>
+<row name="name" null="1" autoincrement="0">
+<datatype>VARCHAR(50)</datatype>
+<default>NULL</default>
+</row>
+<row name="amount" null="1" autoincrement="0">
+<datatype>INTEGER</datatype>
+<default>NULL</default>
+</row>
+<row name="type" null="1" autoincrement="0">
+<datatype>TINYINT(1)</datatype>
+<default>0</default>
+<comment>0 - Income, 1 - Outcome</comment>
+</row>
+<row name="created" null="1" autoincrement="0">
+<datatype>DATETIME</datatype>
+<default>NULL</default>
+</row>
+<row name="updated" null="1" autoincrement="0">
+<datatype>DATETIME</datatype>
+<default>NULL</default>
+</row>
+<row name="deleted" null="1" autoincrement="0">
+<datatype>TINYINT</datatype>
+<default>NULL</default>
+<comment>0 - not delete, 1 - delete</comment>
+</row>
+<key type="PRIMARY" name="">
+<part>id</part>
+</key>
+</table>
+</sql>
